9c360779cee920c1c4ab6c713c503028a5fed325
17Doc <the17docter@icloud.com>
2025-10-22 08:53:26 -0400

    added comments and logic

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+//this is how you comment btw
+
+//deck initializer
+const deck = new Array();
 
 
+//funny down arrow sound
 document.addEventListener('keydown', function(event) {
   if (event.code == 'ArrowDown') {
     playSoundvb()
   }
 });
 
+
+//make card function
 function Card(val, suit, face) {
   this.val = val;
   this.suit = suit;
   this.face = face;
 }
 
+
+//making cards
 const card1 = new card(1, "Heart", 3);
 const card2 = new card(2, "Heart", 4);
 const card3 = new card(3, "Heart", 5);
@@ -25,15 +34,27 @@ const card10 = new card(10, "Heart", "Q");
 const card11 = new card(11, "Heart", "K");
 const card12 = new card(12, "Heart", "A");
 const card13 = new card(13, "Heart", 2);
+
 const card53 = new card("?", "All", "Joker");
 const card54 = new card("?", "All", "Joker");
 const card55 = new card("?", "All", "Joker");
 const card56 = new card("?", "All", "Joker");
 
 
-
+//sound effect
 function playSoundvb() {
 	let ding = new Audio('vineboom.mp3');
 	ding.play();
 }
 
+//reset deck
+function deckReset() {
+	deck.push(card1,card2,card3,card4,card5,card6,card7,card8,card9,card10,card11,card12,card13,card53);
+}
+
+
+//return a random number into an array.
+//removes it from deck, intended to add to a hand array
+function getRandomCard() {
+	return deck.slice(Math.floor(Math.random() * 14););
+}
